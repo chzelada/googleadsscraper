ac546096147d7c5f2a854baa12e0068510b74e5c
chzelada <chzelada@gmail.com>
2017-08-03 07:48:49 -0600

    Commented Code
    Just added a a few comments to the code.

--- googlesearchmultiplescrap.R
+++ googlesearchmultiplescrap.R
@@ -4,11 +4,21 @@ library(curl)
 library(dplyr)
 library(tm)
 library(wordcloud)
-library(regex)
 library(readr)
-ua <- read_csv("ua.csv",col_names = FALSE)
-uule <- read_csv("uule.csv")
+
+## Catalog of UA and UULE
+#UA is used to change User Interface this will help to not get the ip block
+#from google
+ua <- read_csv("ua.csv",col_names = FALSE) 
 names(ua)<-c("id","string")
+
+# UULE is used to change the region of the search, For example,
+# if you want to search ads as someone from florida you need
+# the UULE code for florida. The file uule.csv has all the uule
+# this was scrapped from google 
+uule <- read_csv("uule.csv")
+
+# this function will help up clean the corpus
 clean_corpus <- function(corpus){
   corpus <- tm_map(corpus, stripWhitespace)
   corpus <- tm_map(corpus, removePunctuation)
@@ -18,6 +28,7 @@ clean_corpus <- function(corpus){
   return(corpus)
 }
 
+# with this function we generate the word cloud
 getWordCloud <- function(x){
   AdsTitleSrc<-VectorSource(x)
   AdsTitleCorpus <- VCorpus(AdsTitleSrc)
@@ -30,23 +41,28 @@ getWordCloud <- function(x){
   return(AdsTitleDf)
 }
 
+# Initializing vectors
 AdsSnippet<-c()
 AdsUrlTot<-c()
 AdsTitleTol<-c()
 Stitles<-c()
 Surl<-c()
 Ssnippet<-c()
+
+# State from where you want to make the search
+# KeyWord you want to scrap
+state<-'FL'
+KeyWord<-c("medicare supplemental")
+
+# Start Scrapping
 n=1
 for(i in 1:5){
   print(i)
-  Sys.sleep(sample(1, 1) * 0.3)
+  Sys.sleep(sample(1, 1) * 0.3) ## Random wait to avoid been detected as machine by google.
   index<-sample(1:11,1)
   uar<-ua$string[index]
-  state<-'FL'
   StateName<-state.name[which(state.abb==state)]
   StateUule<-uule$uule[uule$Name==StateName]
-  
-  KeyWord<-c("medicare supplemental")
   KeyWord<-strsplit(KeyWord,split = " ",fixed = TRUE) %>% unlist() 
   xtrakw<-KeyWord
   KeyWord<-paste(KeyWord,collapse = "+")
@@ -54,6 +70,7 @@ for(i in 1:5){
   url<-paste("https://www.google.com/search?q=",trimws(KeyWord),"&ip=0.0.0.0","&uule=",(StateUule),sep="",collapse = "")
   
   html <- read_html(curl(url,handle = new_handle("useragent" = uar)))
+  ### Here we star to scrap by css components
   
   ###Organic
   titles <- html_nodes(html, "#search h3.r")
@@ -92,16 +109,15 @@ for(i in 1:5){
 }
 
 ads_data <- list(url=Surl,
-ssnippet=Ssnippet,
-stitles=Stitles,
-aurl=AdsUrlTot,
-atitle=AdsTitleTol,
-asnippet=AdsSnippet,
-state = state,
-keyword=KeyWord,
-date = Sys.time(),
-searches = n
-)
+                 ssnippet=Ssnippet,
+                 stitles=Stitles,
+                 aurl=AdsUrlTot,
+                 atitle=AdsTitleTol,
+                 asnippet=AdsSnippet,
+                 state = state,
+                 keyword=KeyWord,
+                 date = Sys.time(),
+                 searches = n)
 
 WordCloudData<-getWordCloud(unique(ads_data$asnippet))
 
